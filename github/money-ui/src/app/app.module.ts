a5eff78653ff33c1503042d9dc1100301be08d75
Rafardg <rafael.rodrigues7715@gmail.com>
2022-10-06 11:30:27 -0300

    SideBar Dashboard

--- github/money-ui/src/app/app.module.ts
+++ github/money-ui/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { DashboardModule } from './pages/dashboard/dashboard.module';
 import { LoginComponent } from './pages/seguranca/login/login.component';
 import { PrimengModule } from './primeng.module';
 import { NgModule } from '@angular/core';
@@ -7,18 +8,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { CommonModule } from '@angular/common';
+import { NavbarComponent } from './pages/shared/navbar/navbar.component';
 @NgModule({
   declarations: [
-    AppComponent, LoginComponent
+    AppComponent, LoginComponent, NavbarComponent
   ],
   imports: [
     CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     AppRoutingModule,
-    PrimengModule
+    PrimengModule,
+    DashboardModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [NavbarComponent]
 })
 export class AppModule { }
